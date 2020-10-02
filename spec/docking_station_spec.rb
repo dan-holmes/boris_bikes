6f2576355bde9314b3e5d62010b74ae6893b0ead
Dan Holmes <dan@Dans-MacBook-Air.local>
2020-10-02 14:34:33 +0100

    group tests together

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,43 +1,47 @@
 require "classes"
 
 describe DockingStation do
-  it "can dock and release a bike" do
-    docking_station = DockingStation.new
-    bike = Bike.new
-    docking_station.dock(bike)
-    expect(docking_station.release_bike).to eq bike
+  describe "# docking and releasing" do
+    it "can dock and release a bike" do
+      docking_station = DockingStation.new
+      bike = Bike.new
+      docking_station.dock(bike)
+      expect(docking_station.release_bike).to eq bike
+    end
+
+    it "can dock and release multiple bikes" do
+      docking_station = DockingStation.new
+      test_bikes = []
+      docking_station.capacity.times { test_bikes.push(Bike.new) }
+      test_bikes.each { |bike| docking_station.dock(bike) }
+      released_bikes = []
+      docking_station.capacity.times { released_bikes.push(docking_station.release_bike) }
+      expect(test_bikes).to eq released_bikes.reverse
+    end
   end
 
-  it "can dock and release multiple bikes" do
-    docking_station = DockingStation.new
-    test_bikes = []
-    docking_station.capacity.times { test_bikes.push(Bike.new) }
-    test_bikes.each { |bike| docking_station.dock(bike) }
-    released_bikes = []
-    docking_station.capacity.times { released_bikes.push(docking_station.release_bike) }
-    expect(test_bikes).to eq released_bikes.reverse
-  end
+  describe "# Errors if full or empty" do
+    it "will not dock a bike if full" do
+      docking_station = DockingStation.new
+      docking_station.capacity.times { docking_station.dock(Bike.new) }
+      expect { docking_station.dock(Bike.new) }.to raise_error("Docking station full.")
+    end
 
-  it "will not dock a bike if full" do
-    docking_station = DockingStation.new
-    docking_station.capacity.times { docking_station.dock(Bike.new) }
-    expect { docking_station.dock(Bike.new) }.to raise_error("Docking station full.")
-  end
+    it "will not release a bike if empty" do
+      docking_station = DockingStation.new
+      expect { docking_station.release_bike }.to raise_error "There are no bikes to take out."
+    end
 
-  it "will not release a bike if empty" do
-    docking_station = DockingStation.new
-    expect { docking_station.release_bike }.to raise_error "There are no bikes to take out."
-  end
+    it "can take a custom capacity and not all any more bikes to be docked" do
+      docking_station = DockingStation.new(15)
+      15.times { docking_station.dock(Bike.new) }
+      expect { docking_station.dock(Bike.new) }.to raise_error "Docking station full."
+    end
 
-  it "can take a custom capacity and not all any more bikes to be docked" do
-    docking_station = DockingStation.new(15)
-    15.times { docking_station.dock(Bike.new) }
-    expect { docking_station.dock(Bike.new) }.to raise_error "Docking station full."
-  end
-
-  it "has a fault capacity of 20 if no capacity given" do
-    docking_station = DockingStation.new
-    20.times { docking_station.dock(Bike.new) }
-    expect { docking_station.dock(Bike.new) }.to raise_error "Docking station full."
+    it "has a default capacity of 20 if no capacity given" do
+      docking_station = DockingStation.new
+      20.times { docking_station.dock(Bike.new) }
+      expect { docking_station.dock(Bike.new) }.to raise_error "Docking station full."
+    end
   end
 end

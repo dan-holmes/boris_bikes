ee3aba966f295a5cac97d641f193092bc5b012cd
Dan Holmes <dan@Dans-MacBook-Air.local>
2020-10-02 14:29:17 +0100

    add and test customer capacity

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -29,24 +29,9 @@ describe DockingStation do
     expect { docking_station.release_bike }.to raise_error "There are no bikes to take out."
   end
 
-  # describe "docking station full" do
-  #   before do
-  #     docking_station.bikes_docked = []
-  #     docking_station.default_capacity.times { docking_station.dock(Bike.new) }
-  #   end
-  #   it { expect { docking_station.dock(bike) }.to raise_error("Docking station full.") }
-  # end
-
-  # describe "docking station is empty" do
-  #   before do
-  #     docking_station.bikes_docked = []
-  #   end
-  #   it { expect { docking_station.release_bike }.to raise_error("There are no bikes to take out.") }
-  # end
-
-  # describe "# setting a custom capacity" do
-  #   custom_docking_station = DockingStation.new(15)
-  #   15.times { custom_docking_station.dock(Bike.new) }
-  #   expect { custom_docking_station.dock(Bike.new) }.to raise_error("Docking station is full.")
-  # end
+  it "can take a custom capacity and not all any more bikes to be docked" do
+    docking_station = DockingStation.new(15)
+    15.times { docking_station.dock(Bike.new) }
+    expect { docking_station.dock(Bike.new) }.to raise_error "Docking station full."
+  end
 end
